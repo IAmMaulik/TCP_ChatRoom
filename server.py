e717e29d9c1dfcf7113f7b9412407f8cf57a0865
Maulik Shah <maulik.shah715@gmail.com>
2020-12-01 22:16:42 +0530

    Added a basic handle function

--- server.py
+++ server.py
@@ -7,3 +7,24 @@ port = 55555
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
+
+clients = []
+nicknames = []
+
+# We can use this broadcast function to send messages to all the clients in the chatroom
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nkname = nicknames[index]
+            print(f"{nkname} has left the chat!")
+            nicknames.remove[nkname]
