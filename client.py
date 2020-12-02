56e2510e58c735f209771dc387fa59dd6587597d
Maulik Shah <maulik.shah715@gmail.com>
2020-12-02 15:20:05 +0530

    Added a basic recieve message function

--- client.py
+++ client.py
@@ -1,5 +1,20 @@
 import socket
 import threading
 
+nickname = str(input("Enter your nickname"))
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('169.254.15.253'), 55555)
+
+def recieve():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message=='NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An unknown error occurred")
+            client.close()
+            break
