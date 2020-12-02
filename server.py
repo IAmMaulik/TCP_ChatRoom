74a62631935c587d91d3d9d4aec4d47c9d90daeb
Maulik Shah <maulik.shah715@gmail.com>
2020-12-02 15:05:37 +0530

    Finished the basic version of the server

--- server.py
+++ server.py
@@ -28,3 +28,25 @@ def handle(client):
             nkname = nicknames[index]
             print(f"{nkname} has left the chat!".encode('ascii'))
             nicknames.remove(nkname)
+            break
+
+def recieve():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {client}")
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} has joined the chat!".encode('ascii'))
+        client.send("Connected to the server".encode('ascii'))
+
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+recieve()
