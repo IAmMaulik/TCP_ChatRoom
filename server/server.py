0eb39c92f433fd04d6de5eafbeeae182ffeae4a2
Maulik Shah <maulik.shah715@gmail.com>
2021-01-14 13:22:14 +0530

    Added the main server logic

--- server/server.py
+++ server/server.py
@@ -1,11 +1,64 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
+import time
+from person import Person
 
-# Main variables for the server
+# Glocal constants
 HOST = 'localhost'
 PORT = 5500
-BUFFSIZ = 1024
+BUFSIZ = 1024
 ADDR = (HOST, PORT)
+MAX_CONNECTIONS = 10
+
+# Global Variables
+persons = []
+
+
+def broadcast(msg, name):
+    for person in persons:
+        client = person.client
+        client.send(bytes(name + ": ", "utf8") + msg)
+
+
+def client_communication(person):
+    client = person.client
+
+    name = client.recv(BUFSIZ).decode("utf8")
+    msg = f"{name} has joined the chat!"
+    broadcast(msg, "")
+    
+    while True:
+        try:
+            msg = client.recv(BUFSIZ)
+            if msg == bytes("{quit}", "utf8"):
+                broadcast(f"{name} has left the chat!", "")
+                client.send(bytes("{quit}", "utf8"))
+                client.close()
+                persons.remove(person)
+
+            else:
+                broadcast(msg, name)
+        
+        except Exception as e:
+            print("[EXEPTION] {e}")
+
+def wait_for_connection():
+    run = True
+    
+    while run:
+        try:
+            client, addr = SERVER.accept()
+            person = Person(addr, client)
+            persons.append(person)
+            print(f"[CONNECTION] {addr} connected to the server at {time.time()}")
+            Thread(target=client_communication, args=(person)).start()
+    
+        except Exception as e:
+            print(f"[EXEPTION] {e}")
+            run = False
+
+    print("SERVER CRASHED")
+
 
 # Initializing the server
 SERVER = socket(AF_INET, SOCK_STREAM)
@@ -13,7 +66,7 @@ SERVER.bind(ADDR)
 
 
 if __name__ == "__main__":
-    SERVER.listen(5)
+    SERVER.listen(MAX_CONNECTIONS)
     print("Waiting for connection...")
     ACCEPT_THREAD = Thread(target=wait_for_connection)
     ACCEPT_THREAD.start()
